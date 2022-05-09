0ef4b8d7e0be5c10d31119ec71017eb0d0293eb7
yThiagoFS <99218990+yThiagoFS@users.noreply.github.com>
2022-05-09 00:18:14 -0300

    actualizations in the section specialists

--- assets/script/anim.js
+++ assets/script/anim.js
@@ -3,7 +3,7 @@ const target = document.querySelectorAll('[data-animation]')
 const animClass = 'active'
 
 function animationScroll(){
-    const windowTop = window.pageYOffset - ((window.innerHeight * 0.75) - 350)
+    const windowTop = window.pageYOffset 
     target.forEach( (element) => {
        if( (windowTop) > element.offsetTop ){
            element.classList.add(animClass)
